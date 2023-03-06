f9417887e81fefd899a344303541856186c36ae5
DJ-Wolfy <83953396+DJ-Wolfy@users.noreply.github.com>
2023-03-06 14:04:21 +1100

    removed some more useless files

--- dice.lua
+++
@@ -1,4 +0,0 @@
-changedir("./projects/xbs-3")
-require("use_python_module")
-import("random")
-print(tostring(random.randint(1,6)))
--- use_python_module.lua
+++
@@ -1,5 +0,0 @@
---import any python module in horizon
-function import(mod)
-pyexec("import "+mod)
-pyexec("lua_wrap(\""+mod+"\","+mod)
-end
